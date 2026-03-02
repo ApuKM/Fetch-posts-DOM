85e1809bc30249dfb8550976b9d500db2eb6c8b9
ApuKM <apukumar180@gmail.com>
2026-03-03 00:25:44 +0600

    Added styling to main and the post divs

--- posts.js
+++ posts.js
@@ -1,19 +1,25 @@
 const loadPosts = () => {
-    const url = "https://jsonplaceholder.typicode.com/posts";
-    fetch(url)
-    .then(res => res.json())
-    .then(data => {
-        // console.log(data)
-        displayPosts(data)
-    })
-}
+  const url = "https://jsonplaceholder.typicode.com/posts";
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      // console.log(data)
+      displayPosts(data);
+    });
+};
 
 const displayPosts = (posts) => {
-    const postContainer = document.getElementById("posts-container");
-    posts.forEach(post => {
-        // console.log(post.title)
-        const li = document.createElement("li");
-        li.innerText = post.title
-        postContainer.appendChild(li);
-    });
-}
+  const postContainer = document.getElementById("posts-container");
+  postContainer.innerHTML = "";
+  posts.forEach((post) => {
+    // console.log(post.title)
+    const div = document.createElement("div");
+    div.innerHTML = `
+        <div class="bg-blue-400 hover:scale-102 transition-all duration-200 p-4 rounded-md">
+                <h2 class="text-gray-800 font-bold text-xl">${post.title}</h2>
+                <p class="text-gray-700 font-semibold text-sm mt-2">${post.body}</p>
+            </div>
+        `;
+    postContainer.appendChild(div);
+  });
+};
